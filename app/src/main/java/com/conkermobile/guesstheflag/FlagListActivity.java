63dfed922e2f7ca491e62016782f61f5a4e35442
qingy2019 <qingy2019@outlook.com>
2020-07-08 11:35:58 -0700

    change background and gobutton

---
+++ app/src/main/java/com/conkermobile/guesstheflag/FlagListActivity.java
@@ -0,0 +1,41 @@
+package com.conkermobile.guesstheflag;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static com.conkermobile.guesstheflag.App.countryCode;
+import static com.conkermobile.guesstheflag.App.countryNames;
+import static com.conkermobile.guesstheflag.App.flagUrl;
+
+public class FlagListActivity extends Activity {
+
+    ListView flagsListView;
+
+    public void goBack(View view) {
+        Intent intent = new Intent(this, GoActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flag_list);
+        flagsListView = findViewById(R.id.flagsListView);
+
+        final ListView list = findViewById(R.id.flagsListView);
+        ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
+        for (int i=0; i<countryNames.size(); i++) {
+            arrayList.add(new SubjectData(countryNames.get(i), "https://raw.githubusercontent.com/hjnilsson/country-flags/master/png1000px/",flagUrl + countryCode.get(i).toLowerCase() + ".png"));
+        }
+        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+        flagsListView.setAdapter(customAdapter);
+
+
+    }
+}
--- app/src/main/java/com/conkermobile/guesstheflag/GoActivity.java
+++ app/src/main/java/com/conkermobile/guesstheflag/GoActivity.java
@@ -1,17 +1,18 @@
 package com.conkermobile.guesstheflag;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 
 public class GoActivity extends Activity {
 
-    public void learn(View view) {
 
+
+    public void learn(View view) {
+        Intent intent = new Intent(this, FlagListActivity.class);
+        startActivity(intent);
     }
 
     @Override
